d3077d2cb218fb1a1d92c4a6a40c17b1522cbe5f
Tausif Fardin <tausiffardin4@gmail.com>
2023-02-23 18:42:37 +0600

    finalizing

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcryptjs';
+import connectMongo from '@/database/conn';
+import Users from '@/model/Schema';
 export default NextAuth({
   providers: [
     //google provider
@@ -11,6 +15,29 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      async authorize(credentials, req) {
+        connectMongo().catch((error) => {
+          error: 'Connection failed';
+        });
+
+        const result = await Users.findOne({ email: credentials.email });
+        if (!result) {
+          throw new Error('No user found with this email');
+        }
+
+        const checkPassword = await compare(
+          credentials.password,
+          result.password
+        );
+
+        if (!checkPassword || result.email !== credentials.email) {
+          throw new Error("username or password doesn't match");
+        }
+        return result;
+      },
     }),
   ],
 });
